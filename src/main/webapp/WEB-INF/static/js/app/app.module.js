958946c726af82e7590e1dfb9b120c8e8d08b1cc
Danilo C. Graciano <danilocgraciano@gmail.com>
2016-05-01 17:48:12 -0300

    Retirada de Controller
    Controller descnecessário.

--- src/main/webapp/WEB-INF/static/js/app/app.module.js
+++ src/main/webapp/WEB-INF/static/js/app/app.module.js
@@ -4,17 +4,3 @@ app.run(function($http,$window){
 	$http.defaults.headers.common['Auth-Token'] = $window.sessionStorage.getItem('financialToken');
 });
 
-app.controller('DatepickerPopupDemoCtrl', function ($scope) {
-	  
-	  $scope.format = 'dd/MM/yyyy';
-	  
-	  $scope.open1 = function() {
-	    $scope.popup1.opened = true;
-	  };
-
-	  $scope.popup1 = {
-	    opened: false
-	  };
-
-});
-
