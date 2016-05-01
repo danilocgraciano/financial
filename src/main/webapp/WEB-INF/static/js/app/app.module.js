43d2efdaef58e5cfd6c8f5cd93bde21112e11604
Danilo C. Graciano <danilocgraciano@gmail.com>
2016-05-01 14:31:52 -0300

    Inclusão da lib ui.bootstrap
    inclusão de biblioteca para tratamento de componentes visuais.

--- src/main/webapp/WEB-INF/static/js/app/app.module.js
+++ src/main/webapp/WEB-INF/static/js/app/app.module.js
@@ -1,5 +1,20 @@
-var app = angular.module('financial',['ngRoute','ngResource','login','user','type','partner','invoice','ufCombo']);
+var app = angular.module('financial',['ngRoute','ngResource','login','user','type','partner','invoice','ufCombo','ui.bootstrap']);
 
 app.run(function($http,$window){
 	$http.defaults.headers.common['Auth-Token'] = $window.sessionStorage.getItem('financialToken');
 });
+
+app.controller('DatepickerPopupDemoCtrl', function ($scope) {
+	  
+	  $scope.format = 'dd/MM/yyyy';
+	  
+	  $scope.open1 = function() {
+	    $scope.popup1.opened = true;
+	  };
+
+	  $scope.popup1 = {
+	    opened: false
+	  };
+
+});
+
